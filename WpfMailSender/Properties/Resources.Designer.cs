7480bd3221973b56a28fb05c61b41ba413013730
pltknttn <47625365+pltknttn@users.noreply.github.com>
2020-11-27 21:58:25 +0300

    - Переименование проекта

--- WpfMailSender/Properties/Resources.Designer.cs
+++ WpfMailSender/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfTestMailSender.Properties {
+namespace WpfMailSender.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WpfTestMailSender.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfTestMailSender.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfMailSender.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- WpfMailSender/Properties/Settings.Designer.cs
+++ WpfMailSender/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfTestMailSender.Properties {
+namespace WpfMailSender.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]

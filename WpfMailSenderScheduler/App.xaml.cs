30d2a8d333f9030201b35267d77ee88e773fcd35
pltknttn <47625365+pltknttn@users.noreply.github.com>
2020-12-05 02:26:55 +0300

    - Локатор

--- WpfMailSenderScheduler/App.xaml.cs
+++ WpfMailSenderScheduler/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -8,12 +9,42 @@ using System.Threading.Tasks;
 using System.Windows;
 using WpfMailSenderScheduler.Interfaces;
 using WpfMailSenderScheduler.Services;
+using WpfMailSenderScheduler.ViewModels;
 
 namespace WpfMailSenderScheduler
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    public partial class App : Application
+    {
+        private static IHost _hosting;
+
+        public static IHost Hosting
+        {
+            get
+            {
+                if (_hosting != null) return _hosting;
+
+                var hostBuilder = Host.CreateDefaultBuilder(Environment.GetCommandLineArgs());
+                hostBuilder.ConfigureServices(ConfigureServices);
+                _hosting = hostBuilder.Build();
+                return _hosting;
+            }
+        }
+        public static IServiceProvider Services => Hosting.Services;
+
+        private static void ConfigureServices(HostBuilderContext hostBuilder, IServiceCollection services)
+        {
+            services.AddSingleton<IDialogService, WindowDialog>();
+            services.AddTransient<IMailService, DebugMailService>();
+            services.AddTransient<IMailService, SmtpMailService>();
+            services.AddSingleton<MainWindowViewModel>();
+        }
+    }
+    /* 
+    using Microsoft.Extensions.DependencyInjection;
+    
     public partial class App : Application
     {
         private static IServiceProvider _services;
@@ -33,5 +64,6 @@ namespace WpfMailSenderScheduler
             services.AddTransient<IMailService, DebugMailService>();
             services.AddTransient<IMailService, SmtpMailService>();
         }
-    }
+    }   
+     */
 }
---
+++ WpfMailSenderScheduler/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfMailSenderScheduler.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}

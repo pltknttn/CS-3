52adf96e0c6136b2d7d7907142a161e8aec74174
pltknttn <47625365+pltknttn@users.noreply.github.com>
2020-12-10 02:42:48 +0300

    - тестирование методов класса PasswordUtils

---
+++ WpfMailSenderLibraryTest/PasswordUtilsTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WpfMailSenderScheduler.Data;
+
+namespace WpfMailSenderLibraryTest
+{
+    [TestClass]
+    public class PasswordUtilsTest
+    {
+        private string testStr;
+
+        [TestInitialize]
+        public void TestPrepare()
+        {
+            testStr = "abcd";
+        } 
+
+        [TestMethod]
+        public void TestDecodePassword_Successful()
+        { 
+            const string expectedDecodePassword = "bcde";
+            var result = PasswordUtils.Decode(testStr);
+            Assert.AreEqual(expectedDecodePassword, result);
+        }
+
+        [TestMethod]
+        public void TestDecodePassword_Error()
+        { 
+            const string expectedDecodePassword = "dcda";
+            var result = PasswordUtils.Decode(testStr);
+            Assert.AreEqual(expectedDecodePassword, result);
+        }
+
+        [TestMethod]
+        public void TestDecodePassword_Empty()
+        { 
+            const string expectedDecodePassword = "";
+            var result = PasswordUtils.Decode(testStr);
+            Assert.AreEqual(expectedDecodePassword, result);
+        }
+
+        [TestMethod]
+        public void TestEncodePassword_Successful()
+        { 
+            const string expectedDecodePassword = "`abc";
+            var result = PasswordUtils.Encode(testStr);
+            Assert.AreEqual(expectedDecodePassword,result);
+        }
+
+        [TestMethod]
+        public void TestEncodPassword_Error()
+        {
+            const string expectedDecodePassword = "abca";
+            var result = PasswordUtils.Encode(testStr);
+            Assert.AreEqual(expectedDecodePassword, result);
+        }
+    }
+}
---
+++ WpfMailSenderLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfMailSenderLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WpfMailSenderLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31535657-2a83-4511-9bc6-e553f52becfc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

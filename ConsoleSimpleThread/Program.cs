eb45ee4d5d5eeb1228ce9c1f1c7339750cf00193
pltknttn <47625365+pltknttn@users.noreply.github.com>
2020-12-12 19:02:39 +0300

    демо потоков, таймер

---
+++ ConsoleSimpleThread/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleSimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FamiliarityAtCurrentThread();
+            ThreadTimer();
+            
+            Console.WriteLine("Exit key = N");
+
+            while (Console.ReadKey().KeyChar != 'N') { Console.WriteLine(); }
+
+            Console.WriteLine("\n\rExit...");
+            Thread.Sleep(1000);
+        }
+
+        static void FamiliarityAtCurrentThread()
+        {
+            var currentThread = Thread.CurrentThread;  
+
+            Console.WriteLine($"id={currentThread.ManagedThreadId}, name={currentThread.Name} priority={currentThread.Priority}");
+            currentThread.Name = "Base thread";
+            Console.WriteLine($"id={currentThread.ManagedThreadId}, name={currentThread.Name} priority={currentThread.Priority}");
+        }
+
+        static void PrintTime()
+        {
+            var currentDateTime = DateTime.Now;
+            while (true)
+            {
+                Console.Title = $"{currentDateTime:dd.MM.yyyy HH:mm:ss.fff}";
+                Thread.Sleep(100);
+                currentDateTime = DateTime.Now;
+            }
+        }
+
+        static void ThreadTimer()
+        {
+            var timer = new Thread(new ThreadStart(PrintTime))
+            {
+                IsBackground = true,
+                Priority = ThreadPriority.Highest
+            };
+            timer.Start();
+        }
+    }
+}
